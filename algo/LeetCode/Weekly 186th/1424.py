8c5b6fe880f52f6957cac0aad191f8abcc714b07
김재구 <rlaworn1993@gmail.com>
2020-04-26 21:16:26 +0900

    4.26 - 2

--- algo/LeetCode/Weekly 186th/1424.py
+++ algo/LeetCode/Weekly 186th/1424.py
@@ -1,42 +1,33 @@
 def findDiagonalOrder(nums):
-    output = []
-    maxWidth = -1
-    maxHeight = len(nums)
-    for list in nums:
-        maxWidth = max(maxWidth,len(list))
-    for list in nums:
-        if len(list) < maxWidth:
-            for i in range(maxWidth - len(list)):
-                list.append(0)
-    if maxHeight == 1:
-        for elem in nums[0]:
-            output.append(elem)
-        return output
-    for i in range(maxHeight*3):
-        if i < maxHeight:
-            startRow = i
-            startCol = 0
-            for j in range(i+1):
-                if startRow > maxHeight-1 or startCol > maxWidth-1:
-                    break
-                if nums[startRow][startCol] != 0:
-                    output.append(nums[startRow][startCol])
-                startRow -= 1
-                startCol += 1
-        else:
-            startRow = maxHeight-1
-            startCol = i - maxHeight + 1
-            print(startRow)
-            print(startCol)
-            while not(startRow > maxHeight-1 or startCol > maxWidth-1):
-                if nums[startRow][startCol] != 0:
-                    output.append(nums[startRow][startCol])
-                startRow -= 1
-                startCol += 1
-              
-    return output
+    diagonals = {}
+    for i, line in enumerate(nums):
+        for j in range(len(line)):
+            if i+j not in diagonals.keys():
+                diagonals[i+j] = []
+            diagonals[i+j].append(line[j])
+    ret = []
+    for i in range(len(diagonals)):
+        ret.extend(diagonals[i][::-1])
+    return ret
 
+# def findDiagonalOrder(self, nums: List[List[int]]) -> List[int]:
+#         # we create a dic indexed by sum i+j, as all items in a diagonal will have the same sum.
+#         diagonals = collections.defaultdict(list)
+        
+#         # traverse from top row to bottom; we can see this is reversed order compared to our desired result.
+#         for i, line in enumerate(nums):
+#             for j in range(len(line)):
+#                 diagonals[i+j].append(line[j]) # add the element to the correct bucket.
+#         i = 0
+#         ret = []
+        
+#         # we now reverse the buckets and concatenate them to get the result.
+#         while i in diagonals:
+#             ret.extend(diagonals[i][::-1])
+#             i += 1
+#         return ret
 
 #print(findDiagonalOrder([[1,2,3,4,5],[6,7],[8],[9,10,11],[12,13,14,15,16]]))
 #print(findDiagonalOrder([[1,2,3],[4],[5,6,7],[8],[9,10,11]]))
-print(findDiagonalOrder([[3,3,18],[4,15,11],[19,11,14,14,3]]))
+#print(findDiagonalOrder([[3,3,18],[4,15,11],[19,11,14,14,3]]))
+print(findDiagonalOrder([[1],[2],[3]]))
