50ea6e49f15d194b13fd275b7861cc38bdb056eb
김재구 <rlaworn1993@gmail.com>
2020-04-28 22:21:54 +0900

    4.28 - 2

---
+++ algo/Codeforces/PresentFromLena.py
@@ -0,0 +1,21 @@
+#44min
+n = int(input()) * 2 + 1
+map = []
+lastIdx = []
+for i in range(n):
+    map.append([' ']*n)
+for i in range(int(n/2)+1):
+    num = 0
+    for j in range(int(n/2)-i,int(n/2)+1):
+        map[i][j] = str(num)
+        map[i][n-1-j] = str(num)
+        num+=1
+    map[n-1-i] =  map[i].copy()
+
+for line in map:
+    str = ''.join(line).rstrip()
+    for i in range(len(str)):
+        if i == len(str)-1:
+            print(str[i])
+        else:
+            print(str[i],end=' ')
