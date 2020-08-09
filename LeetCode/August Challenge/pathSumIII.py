51b514ee4fa342da5b77f4ea0380cd9baf01b4a0
김재구 <rlaworn1993@gmail.com>
2020-08-09 20:39:47 +0900

    08.09

--- LeetCode/August Challenge/pathSumIII.py
+++ LeetCode/August Challenge/pathSumIII.py
@@ -4,36 +4,26 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+#경우를 두가지로 나누어서 깔끔한 풀이
 from collections import defaultdict
 class Solution:
-    def getNumOfPath(self,root,accumSum,parent):
-        if root.left == None and root.right == None:
-            if root.val+accumSum == self.sum:
-                self.dict[(parent,root,accumSum)] = 1
-               # print('leaf node - parent val : {}, node val : {}, accumSum : {}'.format(parent.val if parent!= None else "None",root.val,accumSum))
-                return 1
-            else:
-                return 0
+    def getNumOfPath(self,root,accumSum):
+        if root == None:
+            return 0
+        res = 0
         accumSum += root.val
-        leftNewSum = 0
-        rightNewSum = 0
-        if self.dict[(root.left)] == 0:
-            self.dict[(root.left)] = 1
-            leftNewSum += self.getNumOfPath(root.left,0,None) if root.left != None else 0
-        if self.dict[(root.right)] == 0:
-            self.dict[(root.right)] = 1
-            rightNewSum += self.getNumOfPath(root.right,0,None) if root.right != None else 0
-        leftAccumSum = self.getNumOfPath(root.left,accumSum,root) if root.left != None else 0
-        rightAccumSum = self.getNumOfPath(root.right,accumSum,root) if root.right != None else 0
+        #print('{},accumSum : {}, sum : {}'.format(root.val,accumSum,self.sum))
         if accumSum == self.sum:
-            #print('parent val : {}, node val : {}, accumSum : {}'.format(parent.val if parent != None else "None",root.val,accumSum))
-            return 1+leftNewSum+rightNewSum+leftAccumSum+rightAccumSum
-        return leftNewSum+rightNewSum+leftAccumSum+rightAccumSum
+           # print('find!')
+            res+=1
+        res+=self.getNumOfPath(root.left,accumSum)
+        res+=self.getNumOfPath(root.right,accumSum)
+        return res
     
     def pathSum(self, root: TreeNode, sum: int) -> int:
         if root == None:
             return 0
         self.sum = sum
-        self.dict = defaultdict(int)
-        return self.getNumOfPath(root,0,None)
+        return self.pathSum(root.left,sum)+self.getNumOfPath(root,0)+self.pathSum(root.right,sum)
         
