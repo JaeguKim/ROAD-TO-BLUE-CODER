d2f8b2a65d963ea79dfc9a895bad0d968128567e
김재구 <rlaworn1993@gmail.com>
2020-08-13 23:43:57 +0900

    08.13

--- LeetCode/August Challenge/iteratorForCombination.py
+++ LeetCode/August Challenge/iteratorForCombination.py
@@ -31,3 +31,20 @@ class CombinationIterator:
             return True
         else:
             return False
+
+# itertools.combinations 
+# Logic 1: Using itertools to get all combinations - 100 pass 86% faster
+class CombinationIterator:
+
+    def __init__(self, characters: str, combinationLength: int):
+        import itertools
+        # return type of map() -> map object -> can be converted to list or set
+        self.combinations = list(map("".join, itertools.combinations(characters, r=combinationLength)))
+
+    def next(self) -> str:
+        return self.combinations.pop(0)
+
+    def hasNext(self) -> bool:
+        if self.combinations:
+            return True
+        return False
