1cc59e0b16aab4b54e2bb70f4644f9213620ee9a
김재구 <rlaworn1993@gmail.com>
2020-09-05 21:46:13 +0900

    09.05

---
+++ LeetCode/September Challenge/AllElementsInTwoBinarySearchTrees.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+     
+    List<Integer> nums = new ArrayList();
+    
+    void inorder(TreeNode node){
+        if (node == null)
+            return;
+        if (node != null){
+            nums.add(node.val);
+        }
+        if (node.left != null){
+            inorder(node.left);
+        }
+        if (node.right != null){
+            inorder(node.right);
+        }
+    }
+    
+    public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+        inorder(root1);
+        inorder(root2);
+        nums.sort(null);
+        return nums;
+    }
+}
---
+++ LeetCode/September Challenge/PartitionLabel.java
@@ -0,0 +1,21 @@
+// Space Complexity = O(26)
+// Time Complexity = O(N), N : length of S
+class Solution {
+    public List<Integer> partitionLabels(String S) {
+        int[] endIdx = new int[26];
+        for (int i=0; i<S.length(); i++){
+            endIdx[S.charAt(i)-'a'] = i;
+        }
+
+        int start = 0, end = 0;
+        List<Integer> result = new ArrayList();
+        for (int i=0; i<S.length(); i++) {
+            end = Math.max(end, endIdx[S.charAt(i)-'a']);
+            if (i == end){
+                result.add(end-start+1);
+                start = i+1;
+            }
+        }
+        return result;
+    }
+}
