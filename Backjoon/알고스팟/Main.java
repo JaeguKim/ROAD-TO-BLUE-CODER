b6013c0adcc9761cfcb4190523548c631260eddf
김재구 <rlaworn1993@gmail.com>
2020-10-07 12:53:01 +0900

    10.07

--- Backjoon/알고스팟/Main.java
+++ Backjoon/알고스팟/Main.java
@@ -3,6 +3,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.HashSet;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
@@ -35,13 +36,13 @@ public class Main {
         StringTokenizer tokenizer = new StringTokenizer(br.readLine());
         int m = Integer.parseInt(tokenizer.nextToken());
         int n = Integer.parseInt(tokenizer.nextToken());
-        short[][] dist = new short[n + 1][m + 1];
+        int[][] dist = new int[n + 1][m + 1];
         int[][] dp = new int[n + 1][m + 1];
-        //HashSet<String> visited = new HashSet<>();
+        HashSet<String> visited = new HashSet<>();
         for (int i = 0; i < n; i++) {
             String input = br.readLine();
             for (int j = 0; j < m; j++) {
-                dist[i + 1][j + 1] = Short.parseShort(input.substring(j, j + 1));
+                dist[i + 1][j + 1] = Integer.parseInt(input.substring(j, j + 1));
                 dp[i + 1][j + 1] = INF;
             }
         }
@@ -51,13 +52,10 @@ public class Main {
         int[][] offset = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
         while (!pq.isEmpty()) {
             Node front = pq.poll();
-            if (front.row == n && front.col == m) {
-                break;
-            }
-            //String key = String.format("%s%s", front.row, front.col);
-            // if (visited.contains(key))
-            //     continue;
-            // visited.add(key);
+            String key = String.format("%s|%s", front.row, front.col);
+             if (visited.contains(key))
+                 continue;
+             visited.add(key);
             for (int i = 0; i < 4; i++) {
                 int newR = front.row + offset[i][0];
                 int newC = front.col + offset[i][1];
@@ -69,7 +67,6 @@ public class Main {
                     }
                 }
             }
-
         }
         bw.write(dp[n][m] + "");
         bw.flush();
---
+++ HackerRank/JavaVarArgs/Solution.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.lang.reflect.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+class Add {
+    public void add(Integer... nums){
+        String res = nums[0].toString();
+        int sum = nums[0];
+        for (int i=1;i<nums.length;i++){
+            res += String.format("+%d",nums[i]);
+            sum += nums[i];
+        }
+        System.out.println(String.format("%s=%d",res,sum));
+    }
+}
+public class Solution {
+
+    public static void main(String[] args) {
+       try{
+			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			int n1=Integer.parseInt(br.readLine());
+			int n2=Integer.parseInt(br.readLine());
+			int n3=Integer.parseInt(br.readLine());
+			int n4=Integer.parseInt(br.readLine());
+			int n5=Integer.parseInt(br.readLine());
+			int n6=Integer.parseInt(br.readLine());
+			Add ob=new Add();
+			ob.add(n1,n2);
+			ob.add(n1,n2,n3);
+			ob.add(n1,n2,n3,n4,n5);	
+			ob.add(n1,n2,n3,n4,n5,n6);
+			Method[] methods=Add.class.getDeclaredMethods();
+			Set<String> set=new HashSet<>();
+			boolean overload=false;
+			for(int i=0;i<methods.length;i++)
+			{
+				if(set.contains(methods[i].getName()))
+				{
+					overload=true;
+					break;
+				}
+				set.add(methods[i].getName());
+				
+			}
+			if(overload)
+			{
+				throw new Exception("Overloading not allowed");
+			}
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+		
+	
+	
+
+}
+
