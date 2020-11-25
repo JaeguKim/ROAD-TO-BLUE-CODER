224d40e538aa333213c5946fa2452dfaa1d8b913
김재구 <rlaworn1993@gmail.com>
2020-11-25 14:28:15 +0900

    Update Solution.java

--- LeetCode/November Challenge/Basic Calculator ||/Solution.java
+++ LeetCode/November Challenge/Basic Calculator ||/Solution.java
@@ -1,6 +1,6 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Stack;
 
+/*
 class Solution {
     public int calculate(String s) {
         List<Character> opList = new ArrayList<>();
@@ -42,5 +42,45 @@ class Solution {
             }
         }
         return res;
+    }
+}
+*/
+
+/* Stack 한개를 이용한 풀이 */
+// 스택에는 숫자들만 넣고 피연산자들을 합하는 방식
+class Solution {
+    public int calculate(String s) {
+        int len = s.length();
+        if (s == null || len == 0) return 0;
+        Stack<Integer> stack = new Stack<Integer>();
+        int currentNumber = 0;
+        char operation = '+';
+        for (int i=0; i<len; i++){
+            char currentChar = s.charAt(i);
+            if (Character.isDigit(currentChar)) {
+                currentNumber = (currentNumber * 10) + (currentChar - '0');
+            }
+            if (!Character.isDigit(currentChar) && !Character.isWhitespace(currentChar) || i == len - 1) {
+                if (operation == '-') {
+                    stack.push(-currentNumber);
+                }
+                else if (operation == '+') {
+                    stack.push(currentNumber);
+                }
+                else if (operation == '*') {
+                    stack.push(stack.pop() * currentNumber);
+                }
+                else if (operation == '/') {
+                    stack.push(stack.pop() / currentNumber);
+                }
+                operation = currentChar;
+                currentNumber = 0;
+            }
+        }
+        int result = 0;
+        while (!stack.isEmpty()) {
+            result += stack.pop();
+        }
+        return result;
     }
 }
