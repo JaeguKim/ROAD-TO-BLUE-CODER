6d93b1dd6c64373b6666303ca60bf52457fe25c2
김재구 <rlaworn1993@gmail.com>
2021-03-02 21:44:20 +0900

    2021-03-02

---
+++ LeetCode/2021/src/main/java/com/leetcode/marchchallenge/Solution.java
@@ -0,0 +1,16 @@
+package com.leetcode.marchchallenge;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        int size = candyType.length;
+        Set<Integer> set = new HashSet<>();
+        for (int elem : candyType) {
+            set.add(elem);
+        }
+        int type = set.size();
+        return Math.min(size/2,type);
+    }
+}
--- TEST/Main.java
+++
@@ -1,9 +0,0 @@
-class Main {
-    
-
-    public static void main(String[] args) throws Exception {
-        String s = "a";
-        String res = s.substring(1);
-        System.out.println(res);
-    }
-}
