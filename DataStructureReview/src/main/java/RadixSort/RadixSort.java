f4c277439d507df542ae47b5472d4468b0a34e5e
김재구 <rlaworn1993@gmail.com>
2021-05-25 21:04:11 +0900

    05-25

---
+++ DataStructureReview/src/main/java/RadixSort/RadixSort.java
@@ -0,0 +1,30 @@
+package RadixSort;
+
+import java.util.Arrays;
+public class RadixSort {
+
+    static void countSort(int arr[], int n, int exp) {
+        int output[] = new int[n];
+        int i;
+        int count[] = new int[10];
+        for (i = 0; i < n; i++) count[(arr[i] / exp) % 10]++;
+        for (i = 1; i < 10; i++) count[i] += count[i-1];
+        for (i = n-1; i >= 0; i--) {
+            output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+            count[(arr[i] / exp) % 10]--;
+        }
+        for (i = 0; i < n; i++) arr[i] = output[i];
+    }
+
+    static void radixSort(int arr[], int n) {
+        int m = Arrays.stream(arr).max().getAsInt();
+        for (int exp = 1; m / exp > 0; exp *= 10) countSort(arr,n,exp);
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 170, 45, 75, 90, 802, 24, 2, 66 };
+        int n = arr.length;
+        radixSort(arr,n);
+        for (int i=0;i<arr.length;i++) System.out.print(arr[i] + " ");
+    }
+}
