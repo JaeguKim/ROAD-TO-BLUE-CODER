9d1d4c345f3cc1a83ac267cdfc72705788d327a4
JaeguKim <rlaworn1993@gmail.com>
2022-02-24 23:26:38 +0900

    update

--- LeetCode/src/main/java/prob133/Solution.java
+++ LeetCode/src/main/java/prob133/Solution.java
@@ -1,2 +1,43 @@
-package prob133;public class Solution {
+package prob133;
+
+import java.util.*;
+
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+
+public class Solution {
+    public Node cloneGraph(Node node) {
+        if (node == null) return null;
+        Map<Integer,Node> map = new HashMap<>();
+        Queue<Node> que = new LinkedList<>();
+        que.add(node);
+        Node start = new Node(node.val);
+        map.put(start.val, start);
+        while (que.size() > 0) {
+            Node cur = que.poll();
+            Node copiedCur = map.get(cur.val);
+            for (Node adjNode : cur.neighbors) {
+                if (!map.containsKey(adjNode.val)) {
+                    map.put(adjNode.val,new Node(adjNode.val));
+                    que.add(adjNode);
+                }
+                copiedCur.neighbors.add(map.get(adjNode.val));
+            }
+        }
+        return start;
+    }
 }
--- LeetCode/src/test/java/prob133/SolutionTest.java
+++ LeetCode/src/test/java/prob133/SolutionTest.java
@@ -1,2 +1,22 @@
-package prob133;public class SolutionTest {
+package prob133;
+
+import org.junit.Test;
+
+public class SolutionTest {
+
+    @Test
+    public void test() {
+        Solution sol = new Solution();
+        Node node = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        node.neighbors.add(node2);
+        node2.neighbors.add(node);
+        node2.neighbors.add(node3);
+        node3.neighbors.add(node2);
+        node.neighbors.add(node3);
+        node3.neighbors.add(node);
+        Node res = sol.cloneGraph(node);
+
+    }
 }
