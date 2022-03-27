48c2aa4c13bcb88dd5dade816b0516038b59ffa2
JaeguKim <rlaworn1993@gmail.com>
2022-03-27 23:36:50 +0900

    update

--- LeetCode/src/main/java/prob1337/Solution.java
+++ LeetCode/src/main/java/prob1337/Solution.java
@@ -6,6 +6,11 @@ import java.util.stream.IntStream;
 
 public class Solution {
     public int[] kWeakestRows(int[][] mat, int k) {
-        return IntStream.range(0,mat.length).mapToObj(i->new int[]{i, Arrays.stream(mat[i]).sum()}).sorted(Comparator.comparingInt(a -> a[1])).limit(k).mapToInt(a->a[0]).toArray();
+        return IntStream.range(0,mat.length)
+                .mapToObj(i->new int[]{i, Arrays.stream(mat[i]).sum()})
+                .sorted(Comparator.comparingInt(a -> a[1]))
+                .limit(k)
+                .mapToInt(a->a[0]).
+                toArray();
     }
 }
